28ace33f1cdfae12ab1dbbe853607764b5348f44
ティン・ルーフ <tinruufu@gmail.com>
2016-03-26 19:00:49 +0000

    make pictures more udey

--- image.py
+++ image.py
@@ -1,7 +1,9 @@
 import os
 from random import choice
 
-from PIL import Image, ImageDraw, ImageEnhance, ImageFont, ImageOps
+from PIL import (
+    Image, ImageDraw, ImageFilter, ImageEnhance, ImageFont, ImageOps,
+)
 
 
 COLOURS = [
@@ -33,35 +35,59 @@ def get_font(name, size):
     return ImageFont.truetype(os.path.join(ROOT, 'fonts', name), size)
 
 
+def get_mask(size, colour):
+    return Image.new('RGBA', size, colour)
+
+
 def make_image(term, definition):
     canvas = Image.new('RGBA', (SIZE, SIZE))
+    text = Image.new('RGBA', (SIZE, SIZE), (0, 0, 0, 0))
+    text_draw = ImageDraw.Draw(text)
     tile = get_tile()
 
     x = 0
     while x <= SIZE:
         y = 0
         while y <= SIZE:
-            print x, y
             canvas.paste(tile, (x, y))
             y += tile.height
         x += tile.width
 
-    canvas_draw = ImageDraw.Draw(canvas)
     header_font = get_font('Lora-Bold.ttf', PT*7)
     body_font = get_font('DroidSans.ttf', PT*5)
     logo_font = get_font('TypicalWriter.ttf', PT*10)
 
-    canvas_draw.text((PT*4.5, PT*3), term, font=header_font)
-    canvas_draw.text((PT*4.5, PT*90), definition, font=body_font)
+    words = term.split(' ')
+    lines = []
+    while words:
+        line = [words.pop(0)]
+        while words and text_draw.textsize(
+            ' '.join(line + [words[0]]),
+            font=header_font,
+        )[0] < 80*PT:
+            line.append(words.pop(0))
+
+        lines.append(' '.join(line))
+
+    text_draw.multiline_text(
+        (PT*4.5, PT*3), '\n'.join(lines), font=header_font, spacing=PT*2)
+
+    text_draw.text((PT*4.5, PT*90), definition, font=body_font)
 
     b = Image.new('RGBA', (PT*11, PT*10), (0, 0, 0, 0))
     b_draw = ImageDraw.Draw(b)
     b_draw.text((0, 0), 'b', font=logo_font)
-    d = b.transpose(Image.FLIP_LEFT_RIGHT)
-    d_draw = ImageDraw.Draw(d)
-    d_draw.text((0, 0), 'u', font=logo_font)
+    logo = b.transpose(Image.FLIP_LEFT_RIGHT)
+    logo_draw = ImageDraw.Draw(logo)
+    logo_draw.text((0, 0), 'u', font=logo_font)
+    logo = logo.filter(ImageFilter.GaussianBlur(PT/10))
 
-    canvas.paste(d, (PT*84, PT*2), d)
+    canvas.paste(get_mask(logo.size, 'white'), (PT*84, PT*2), logo)
+
+    shadow = text.filter(ImageFilter.GaussianBlur(PT/6))
+
+    canvas.paste(get_mask(text.size, (50, 50, 50)), (PT/4, PT/4), shadow)
+    canvas.paste(get_mask(text.size, 'white'), (0, 0), text)
 
     canvas.save('out.png')
 
