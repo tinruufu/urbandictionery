10140d0cd5164b8ba5ad21cc4a2b94d6a72c2f63
ティン・ルーフ <tinruufu@gmail.com>
2016-04-13 14:49:19 +0100

    have the api be importable

--- tweet.py
+++ tweet.py
@@ -5,15 +5,18 @@ from image import make_image
 from secrets import app_key, app_secret, token_key, token_secret
 
 
+auth = tweepy.OAuthHandler(app_key, app_secret)
+auth.set_access_token(token_key, token_secret)
+api = tweepy.API(auth)
+
+
 def tweet():
     euphamism = generate()
     print euphamism
     status = '{} - masturbating'.format(euphamism)
 
-    auth = tweepy.OAuthHandler(app_key, app_secret)
-    auth.set_access_token(token_key, token_secret)
     image = make_image(euphamism, 'masturbating')
-    tweepy.API(auth).update_with_media(image, status=status)
+    api.update_with_media(image, status=status)
 
 
 if __name__ == '__main__':
